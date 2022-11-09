50d0374770ce34f34a0d1094ff3d319d3a888af3
free0210 <deljjang@gmail.com>
2022-11-09 12:27:05 +0900

    4.mainDisplay

--- app/src/main/java/com/siny/bible/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/siny/bible/ui/screen/main/MainViewModel.kt
@@ -1,34 +1,16 @@
 package com.siny.bible.ui.screen.main
 
-import android.content.Context
-import android.util.Log
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.siny.data.model.MainData
 import com.siny.data.repository.MainRepository
-import kotlinx.coroutines.launch
 
 class MainViewModel(
-    private val repository: MainRepository,
-    private val cd1: String,
+    repository: MainRepository,
+    cd1: String,
 ) : ViewModel() {
 
-    private val tag = javaClass.simpleName
-
     var mainList: List<MainData> = repository.getList(cd1)
-
-    init {
-        viewModelScope.launch {
-            try {
-                Log.d(tag, "$tag.init")
-                mainList = repository.getList(cd1)
-            } catch(e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
 
 }
 
